7e0e2453fa7303985530dc4dc463684b4d201b93
tarikochuery <ttarikechuery@gmail.com>
2021-07-21 18:33:13 -0300

    Componentes atômicos concluídos

---
+++ client/src/components/Atoms/atoms.js
@@ -0,0 +1,46 @@
+import React, {useEffect, useState} from 'react'
+import './style.css'
+import { Link } from 'react-router-dom'
+
+export const Icon = ({image, kind}) => {
+    return(
+        <div className={`icon ${kind}`}>
+            <img src={image} />
+        </div>
+    )
+}
+
+export const ContinueBtn = ({children, kind, route}) => {
+    const [src, setSrc] = useState('assets/images/green-arrow.svg')
+
+    useEffect(() => {
+        if (kind === 'next') {
+            setSrc('assets/images/white-arrow.svg')
+        }
+    },[src])
+
+
+    return(
+        <div style={{width:'296px'}}>
+            <Link to={route} style={{textDecoration: 'none'}}>
+                <div className={`continue-btn ${kind}`}>
+                    <span>{children}</span> <img src={src} /> 
+                </div>
+            </Link>
+        </div>
+    )
+}
+
+export const InputField = ({type, children}) => {
+    const [input, setInput] = useState('')
+    const handleChange = (event) =>{
+        setInput(event.target.value)
+    }
+
+    return(
+        <div className='input'>
+            <label>{children}</label>
+            <input type={type} value={input} onChange={handleChange} />
+        </div>
+    )
+}
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './home.css'
+import { InputField, ContinueBtn, Icon } from '../Atoms/atoms';
 
 const Home = () => {
     return(
-        <>
+        <div id='home' style={{backgroundImage: 'linear-gradient(to left, #67E799, #4AD07E)',
+                                height: '100%' }}>
             <h1>Home</h1>
-        </>
+            <ContinueBtn route='/login' kind='start'>
+                Começar
+            </ContinueBtn>
+            <Icon image='/assets/images/logo.svg' kind='start'/>
+            <InputField type='email' >
+                Email
+            </InputField>
+        </div>
     )
 }
 
