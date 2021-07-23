a05bc53934b8adfb420b113e6758c6bb3b1d91ba
D1360-64RC14 <gdiego416@gmail.com>
2021-07-23 00:04:07 -0300

    Adicionado bcrypt ao user.controller para hash das senhas

--- server/src/api/controllers/user.controller.js
+++ server/src/api/controllers/user.controller.js
@@ -1,23 +1,26 @@
 const TypeORM = require("typeorm");
 const models = require("../../database/models");
+const bcrypt = require("../../config/bcrypt");
 
 /**
  * @param {string} username 
  * @param {string} email 
  * @param {string} password 
  */
-function register(username, email, password) {
+async function register(username, email, password) {
     const userRepo = TypeORM.getRepository(models.User)
+
+    const hashedPassword = await bcrypt.hash(password)
 
     return userRepo.save({
         username,
         email,
-        password,
+        password: hashedPassword,
         created_at: new Date(),
         updated_at: new Date()
     })
 }
 
 module.exports = {
-    register
+    register,
 }
