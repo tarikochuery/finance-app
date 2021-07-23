31308c291d054b9e5e071fbc57a15ac8f3f18b00
D1360-64RC14 <gdiego416@gmail.com>
2021-07-23 00:06:37 -0300

    Removido envio de senhas no response do auth.middleware

--- server/src/api/middlewares/auth.middleware.js
+++ server/src/api/middlewares/auth.middleware.js
@@ -2,23 +2,25 @@ const Express = require("express");
 const controllers = require("../controllers");
 
 /**
- * 
  * @param {Express.Request} req 
  * @param {Express.Response} res 
  * @param {Express.NextFunction} next 
  */
 async function register(req, res, next) {
-    const {
-        username,
-        email,
-        password
-    } = req.body;
+    const { username, email, password } = req.body;
 
     const wroteData = await controllers.user.register(username, email, password);
 
-    res.send(wroteData)
+    res.status(201).send({
+        id:         wroteData.id,
+        username:   wroteData.username,
+        email:      wroteData.email,
+        created_at: wroteData.created_at,
+        updated_at: wroteData.updated_at
+    })
+}
 }
 
 module.exports = {
-    register
+    register,
 }
