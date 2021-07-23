bd1d36e0a5e0215052cca1eba1cfc4957089c3a0
D1360-64RC14 <gdiego416@gmail.com>
2021-07-23 19:20:10 -0300

    Remoção do import do módulo password

--- server/src/api/validators/index.js
+++ server/src/api/validators/index.js
@@ -1,9 +1,7 @@
-const password = require("./password.validator");
 const token = require("./token.validator");
 const auth = require("./auth.validator");
 
 module.exports = {
-    password,
     token,
     auth
 }
