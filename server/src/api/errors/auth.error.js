66c620c7580ff34188652e165ecceab199093c2c
D1360-64RC14 <gdiego416@gmail.com>
2021-07-25 16:54:28 -0300

    Adicionado ao auth.error erros 'invalid_email' e 'invalid_token'

--- server/src/api/errors/auth.error.js
+++ server/src/api/errors/auth.error.js
@@ -1,5 +1,7 @@
 const APIError = require("./APIError")
 
+// TODO: implementar à APIError possibilidade de
+// passar header (código 401 deve retornar WWW-Authenticate)
 const passwordIncorrect = new APIError(401, {
     error: "invalid_password",
     error_description: "A senha está incorreta."
@@ -20,9 +22,21 @@ const emailAlreadyExist = new APIError(409, {
     error_description: "O email já existe."
 })
 
+const emailInvalid = new APIError(400, {
+    error: "invalid_email",
+    error_description: "O email é inválido ou não existe."
+})
+
+const tokenInvalid = new APIError(403, {
+    error: "invalid_token",
+    error_description: "O token é inválido ou expirou."
+})
+
 module.exports = {
     passwordIncorrect,
     emailNotFound,
     passwordLength,
-    emailAlreadyExist
+    emailAlreadyExist,
+    tokenInvalid,
+    emailInvalid
 }
